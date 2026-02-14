fe3604de921f1ef51a275bef271e3cb107ddd958
Adam Rasfeld <arasfeld@gmail.com>
2026-02-14 12:08:15 -0500

    chore: add eslint + typescript-eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    ignores: ["**/node_modules/**", "**/dist/**"],
+  },
+  {
+    files: ["packages/*/src/**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.eslint.json", "./packages/*/tsconfig.json"],
+      },
+    },
+  },
+);
